f0676a8f80a31635d7f3576bea647bfd967aa316
moss chuang <moss.chuang@gmail.com>
2019-10-25 17:44:10 +0800

    test gConnectBlockRunning

--- src/coins.cpp
+++ src/coins.cpp
@@ -49,6 +49,8 @@ static int64_t nShrimp[6] = {0};
 
 CCoinsMap::iterator CCoinsViewCache::FetchCoin(const COutPoint &outpoint) const {
 
+    LogPrintf("[iii] gConnectBlockRunning=%d\n", gConnectBlockRunning);
+
     if (nAbuCount != gAbuliabiachia) {
         LogPrintf("[III] abu=%d ", nAbuCount);
         int64_t nSum = 0;
